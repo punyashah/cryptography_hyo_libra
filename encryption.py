9d442e7396e82fb59c9149fd567dd242408d6807
punyashah <65733108+punyashah@users.noreply.github.com>
2020-06-07 22:55:05 -0400

    Add files via upload

---
+++ encryption.py
@@ -0,0 +1,17 @@
+def encrypt(text,shift):
+    output_str = ""
+
+    for char in text:
+        if(char.isupper()):
+            shifted_final = ord(char) + (shift%26)
+            if(shifted_final > 90):
+                shifted_final -= 26
+            output_str+=chr(shifted_final)
+        elif(char.islower()):
+            shifted_final = ord(char) + (shift%26)
+            if(shifted_final > 122):
+                shifted_final -= 26
+            output_str += chr(shifted_final)
+
+
+    return output_str
